6888febbed835767b7b24bc9f62e42d463955224
1714480752@qq.com <1714480752@.qq.com>
2022-01-24 20:03:44 +0800

    [feature] 新增StartActivity，开始界面

---
+++ app/src/main/java/com/example/couldmusic/page/activity/StartActivity.java
@@ -0,0 +1,16 @@
+package com.example.couldmusic.page.activity;
+
+
+import android.os.Bundle;
+
+import com.example.couldmusic.R;
+import com.example.couldmusic.base.BaseActivity;
+
+public class StartActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
